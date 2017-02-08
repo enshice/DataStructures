f42a3e1af071b9a816f244755143c95d178d9458
enshice <enshice@gmail.com>
2017-02-09 00:50:31 +0800

    Add EnshiceList

---
+++ EnshiceList.h
@@ -0,0 +1,118 @@
+#include <stdlib.h>
+#include <iostream>
+
+namespace enshice
+{
+template<typename Object>
+class List
+{
+
+private: 
+    struct Node
+    {
+        Object data;
+        Node* prev;
+        Node* next;
+
+        Node(const Object& d = Object(), Node* p = NULL, Node* n = NULL)
+        : data(d), prev(p), next(n) {}
+    };
+
+public:
+    class const_iterator
+    {
+    public:
+        const_iterator() : current(NULL) {}
+
+        const Object& operator*() const
+        {
+            return retrieve();
+        }
+
+        const_iterator& operator++()
+        {
+            current = current->next;
+            return *this;
+        }
+
+        const_iterator operator++(int)
+        {
+            const_iterator old = *this;
+            ++(*this);
+            return old;
+        }
+
+        bool operator==(const const_iterator& rhs) const
+        {
+            return current == rhs.current;
+        }
+
+        bool operator!=(const const_iterator& rhs) const
+        {
+            return current != rhs.current;
+        }
+
+    protected:
+        Node* current;
+
+        Object& retrieve() const
+        {
+            return current->data;
+        }
+
+        const_iterator(Node *p): current(p) {}
+        
+        friend class List<Object>;
+    };
+
+    /*
+    class iterator : public const_iterator
+    {
+    public:
+        iterator(){}
+
+        Object& operator*()
+        {
+            return retrieve();
+        }
+
+        const Object& operator*() const
+        {
+            return const_iterator::operator*();
+        }
+
+        iterator& operator++()
+        {
+            current = current->next;
+            return *this;
+        }
+
+        iterator operator++(int)
+        {
+            iterator old = *this;
+            ++(*this);
+            return old;
+        }
+
+    protected:
+        iterator(Node* p) : const_iterator(p) {}
+
+        friend class List<Object>;
+    };
+    */
+    
+public:
+    List() { std::cout << "List constructor." << std::endl;};
+
+    List(const List& rhs) { std::cout << "List copy constructor." << std::endl; };
+
+    ~List() { std::cout << "List ~constructor." << std::endl;};
+
+    const List& operator=(const List& rhs);
+
+private:
+    int theSize;
+    Node* head;
+    Node* tail;
+};
+}
---
+++ EnshiceListTest.cpp
@@ -0,0 +1,13 @@
+#include "EnshiceList.h"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout << "test EnshiceList begin" << endl;
+
+    cout << "test EnshiceList end" << endl;
+
+    return 0;
+}
